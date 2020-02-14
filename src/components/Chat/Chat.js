51adb5acfcef831404870a76315c772479000aba
a-exploit <choudharyritik3@gmail.com>
2020-02-14 14:48:23 +0530

    Fixe Endpoint

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -18,8 +18,8 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  // const ENDPOINT = 'https://chat-app-ayush.herokuapp.com/';
-  const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'https://chat-app-ayush.herokuapp.com/';
+  // const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
