8bdc6877860359559422905574d1b7ae9f952459
a-exploit <choudharyritik3@gmail.com>
2020-02-14 17:00:15 +0530

    pwa improved

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -19,7 +19,7 @@ const isLocalhost = Boolean(
         /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
       )
   );
-const staticCacheName='site-static'
+const staticCacheName='site-static-v2'
 const assets=[
   '/',
   '/index.html'
