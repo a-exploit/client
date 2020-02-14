f07e5b23805dad4bd72518f61846a47cfccea4c9
a-exploit <choudharyritik3@gmail.com>
2020-02-14 14:40:54 +0530

    Added is typing event

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 import './Input.css';
-
-const Input = ({ setMessage, sendMessage, message }) => (
+let timeout=null
+const Input = ({ setMessage, sendMessage, message ,isTyping,isNotTyping}) => (
   <form className="form">
     <input
       className="input"
       type="text"
       placeholder="Type a message..."
       value={message}
-      onChange={({ target: { value } }) => setMessage(value)}
-      onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+      onChange={(event) =>{ 
+        setMessage(event.target.value) 
+        isTyping(event)
+        clearTimeout(timeout)
+        timeout= setTimeout(()=>{
+          isNotTyping(event)
+        },500)
+      }
+      }
+      onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : (event.key===' '?isNotTyping(event):null)}
+      onBlur={event=>isNotTyping(event)}
     />
     <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
   </form>
